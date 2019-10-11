ee48f7141c8b742ef5e8a9fb5dbbd9ad91b6eaf2
Alex <alex_lopez.es@icloud.com>
2019-10-11 21:45:15 +0200

    Initial Commit

---
+++ Toads/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toads
+//
+//  Created by Alex Lopez on 11/10/2019.
+//  Copyright © 2019 Alex Lopez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
