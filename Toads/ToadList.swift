8389f62a7eace06459b29789e1db33c1a3b05e2f
AlexLopezDevelop <alex_lopez.es@icloud.com>
2019-11-16 18:22:25 +0100

    Added: Toad list

---
+++ Toads/ToadList.swift
@@ -0,0 +1,30 @@
+//
+//  ToadList.swift
+//  Toads
+//
+//  Created by Alex Lopez on 16/11/2019.
+//  Copyright © 2019 Alex Lopez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ToadList: View {
+    var body: some View {
+        NavigationView {
+            List(toads) {
+                toad in
+                NavigationLink(destination: ContentView()) {
+                    ToadRow(toad: toad)
+                }
+            }.navigationBarTitle(Text("Toads"), displayMode: .large)
+        }
+    }
+}
+
+#if DEBUG
+struct ToadList_Previews: PreviewProvider {
+    static var previews: some View {
+        ToadList()
+    }
+}
+#endif
